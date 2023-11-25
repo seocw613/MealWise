434316288cf8bc886f44eea461e3a9a5ab8dd841
seocw613 <109450691+seocw613@users.noreply.github.com>
2023-11-25 15:43:04 +0900

    empty project

---
+++ mealwise/src/test/java/com/mealwise/demo/MealwiseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mealwise.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealwiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
